f70328fe22cacd8d5a729317f0bebba62205b68a
myokoyama <part7steelballrun.to.8@gmail.com>
2019-01-02 01:38:14 +0900

    install SQLAlchemy

--- application/flask_blog/config.py
+++ application/flask_blog/config.py
@@ -1,5 +1,7 @@
 # flask_blog/config.py
 
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_blog.db'
+SQLALCHEMY_TRACK_MODIFICATION = True
 DEBUG = True
 SECRET_KEY = 'secret key' # このsecret keyを使って、session情報が暗号化される
 USERNAME = 'john'
