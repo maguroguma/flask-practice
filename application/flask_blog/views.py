64affd8b43541f39d600e10151c1bee305e87f1b
myokoyama <part7steelballrun.to.8@gmail.com>
2019-01-02 00:45:25 +0900

    add login form

--- application/flask_blog/views.py
+++ application/flask_blog/views.py
@@ -6,3 +6,19 @@ from flask_blog import app
 @app.route('/')
 def show_entries():
   return render_template('entries/index.html')  # パスにtemplatesは不要
+
+# methodsはURLに対して許可するHTTPメソッド、デフォルトではGETのみが許可される
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    if request.form['username'] != app.config['USERNAME']:
+      print('ユーザ名が異なります')
+    elif request.form['password'] != app.config['PASSWORD']:
+      print('パスワードが異なります')
+    else:
+      return redirect('/')
+  return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+  return redirect('/')
