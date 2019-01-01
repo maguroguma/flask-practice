9e0e4d217ce9a8c6942af7feb9c260106a080a48
myokoyama <part7steelballrun.to.8@gmail.com>
2019-01-02 01:23:35 +0900

    add session

--- application/flask_blog/config.py
+++ application/flask_blog/config.py
@@ -1,5 +1,6 @@
 # flask_blog/config.py
 
 DEBUG = True
+SECRET_KEY = 'secret key' # このsecret keyを使って、session情報が暗号化される
 USERNAME = 'john'
 PASSWORD = 'due123'
--- application/flask_blog/views.py
+++ application/flask_blog/views.py
@@ -5,20 +5,25 @@ from flask_blog import app
 
 @app.route('/')
 def show_entries():
+  if not session.get('logged_in'):
+    return redirect('/login')
   return render_template('entries/index.html')  # パスにtemplatesは不要
 
 # methodsはURLに対して許可するHTTPメソッド、デフォルトではGETのみが許可される
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+  error = None
   if request.method == 'POST':
     if request.form['username'] != app.config['USERNAME']:
       print('ユーザ名が異なります')
     elif request.form['password'] != app.config['PASSWORD']:
       print('パスワードが異なります')
     else:
+      session['logged_in'] = True
       return redirect('/')
   return render_template('login.html')
 
 @app.route('/logout')
 def logout():
+  session.pop('logged_in', None)  # Flaskではsessionという変数を扱うことでセッション情報を扱うことができる
   return redirect('/')
