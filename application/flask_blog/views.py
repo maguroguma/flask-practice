5cbe93300afec9ddfe109c7aff71aeaf5df1bab7
myokoyama <part7steelballrun.to.8@gmail.com>
2019-01-02 01:28:15 +0900

    add flash messages

--- application/flask_blog/views.py
+++ application/flask_blog/views.py
@@ -15,15 +15,17 @@ def login():
   error = None
   if request.method == 'POST':
     if request.form['username'] != app.config['USERNAME']:
-      print('ユーザ名が異なります')
+      flash('ユーザ名が異なります')
     elif request.form['password'] != app.config['PASSWORD']:
-      print('パスワードが異なります')
+      flash('パスワードが異なります')
     else:
       session['logged_in'] = True
+      flash('ログインしました')
       return redirect('/')
   return render_template('login.html')
 
 @app.route('/logout')
 def logout():
   session.pop('logged_in', None)  # Flaskではsessionという変数を扱うことでセッション情報を扱うことができる
+  flash('ログアウトしました')
   return redirect('/')
