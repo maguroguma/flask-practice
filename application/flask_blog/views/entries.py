2959be86cd1b0b267fe2a7b99a3392bfef571bf5
myokoyama <part7steelballrun.to.8@gmail.com>
2019-01-02 18:32:19 +0900

    add function of posting new entry

--- application/flask_blog/views/entries.py
+++ application/flask_blog/views/entries.py
@@ -2,9 +2,32 @@
 
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+from flask_blog import db
+from flask_blog.models.entries import Entry
 
 @app.route('/')
 def show_entries():
   if not session.get('logged_in'):
     return redirect(url_for('login'))
-  return render_template('entries/index.html')  # templateはパスに含めなくて良い
+  entries = Entry.query.order_by(Entry.id.desc()).all() # データベースからすべての記事を取得し、新しく記事が作られた順に並べる
+  # 2つめの引数により、index.htmlでデータベースから取得した記事すべてがentriesという名前で参照できるようになる
+  return render_template('entries/index.html', entries=entries)  # templateはパスに含めなくて良い
+
+@app.route('/entries/new', methods=['GET'])
+def new_entry():
+  if not session.get('logged_in'):
+    return redirect(url_for('login'))
+  return render_template('entries/new.html')
+
+@app.route('/entries', methods=['POST'])
+def add_entry():
+  if not session.get('logged_in'):
+    return redirect(url_for('login'))
+  entry = Entry(
+    title=request.form['title'],
+    text=request.form['text']
+  )
+  db.session.add(entry)
+  db.session.commit()
+  flash('新しく記事が作成されました')
+  return redirect(url_for('show_entries'))
