ece9a773d5d4909bde85ecbc1faaa904f175bfb6
myokoyama <part7steelballrun.to.8@gmail.com>
2019-01-02 19:00:39 +0900

    add function of updating existing entries

--- application/flask_blog/views/entries.py
+++ application/flask_blog/views/entries.py
@@ -38,3 +38,22 @@ def show_entry(id):
     return redirect(url_for('login'))
   entry = Entry.query.get(id) # 渡されたidの維持をデータベースから取得できる
   return render_template('entries/show.html', entry=entry)
+
+@app.route('/entries/<int:id>/edit', methods=['GET'])
+def edit_entry(id):
+  if not session.get('logged_in'):
+    return redirect(url_for('login'))
+  entry = Entry.query.get(id)
+  return render_template('entries/edit.html', entry=entry)
+
+@app.route('/entries/<int:id>/update', methods=['POST'])
+def update_entry(id):
+  if not session.get('logged_in'):
+    return redirect(url_for('login'))
+  entry = Entry.query.get(id)
+  entry.title = request.form['title']
+  entry.text = request.form['text']
+  db.session.merge(entry) # DBの更新
+  db.session.commit()
+  flash('記事が更新されました')
+  return redirect(url_for('show_entries'))
