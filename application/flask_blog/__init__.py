90895381029b570fba01ae6cb9160c56a8f4eaea
myokoyama <part7steelballrun.to.8@gmail.com>
2019-01-04 18:34:07 +0900

    my first flask CORS

--- application/flask_blog/__init__.py
+++ application/flask_blog/__init__.py
@@ -15,3 +15,6 @@ app.register_blueprint(entry, url_prefix='/users')  # entryアプリケーショ
 app.register_blueprint(api, url_prefix='/api')
 
 from flask_blog.views import views
+
+from flask_cors import CORS
+CORS(app)
